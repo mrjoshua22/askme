3a67987c99ff8b57429268c9c220b6d8731ef5e6
mrjoshua <mrjoshua@gmail.com>
2022-12-22 10:31:28 +0300

    Add email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
-    @user = User.create(user_params)
+    user_params = params.require(:user).permit(
+      :name, :nickname, :email, :password
+    )
+    
+    @user = User.new(user_params)
 
-    session[:user_id] = @user.id
-
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
+      render :new
+    end
   end
 
   def new
