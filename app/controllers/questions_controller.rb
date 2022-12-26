20f630cead9fe4680841f42fa0de5582a1643b46
mr_joshua <ulanov.ng@gmail.com>
2022-12-26 20:17:47 +0300

    Add user#show with view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,14 +2,15 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: %i[update destroy show edit hide]
 
   def new
-    @question = Question.new
+    @user = User.find(params[:user_id])
+    @question = Question.new(user: @user)
   end
 
   def create
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to question_path(@question), notice: 'Новый вопрос создан!'
+      redirect_to user_path(@question.user), notice: 'Новый вопрос создан!'
     else
       flash[:alert] = 'Не удалось создать вопрос!'
       render :new
@@ -21,7 +22,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to question_path(@question), notice: 'Сохранили вопрос!'
+      redirect_to user_path(@question.user), notice: 'Сохранили вопрос!'
     else
       flash.now[:alert] = 'Не удалось обновить вопрос!'
       render :edit
@@ -29,9 +30,10 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+    @user = @question.user
     @question.destroy
 
-    redirect_to questions_path, notice: 'Вопрос удалён!'
+    redirect_to user_path(@user), notice: 'Вопрос удалён!'
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     redirect_to root_path, notice: 'Пользователь удалён'
   end
 
+  def show
+    @user = User.find(params[:id])
+    @questions = @user.questions
+    @question = Question.new(user: @user)
+  end
+
   private
 
   def user_params
