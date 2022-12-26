90b74013dbc6182eeb92822bb9f8f8287bfd702e
mr_joshua <ulanov.ng@gmail.com>
2022-12-26 21:58:25 +0300

    Add restriction for edit, update and destroy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[update destroy show edit hide]
+  before_action :ensure_current_user, only: %i[edit update destroy hide]
+  before_action :set_question_for_current_user, only: %i[edit update destroy hide]
 
   def new
     @user = User.find(params[:user_id])
@@ -37,6 +38,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def index
@@ -52,11 +54,15 @@ class QuestionsController < ApplicationController
 
   private
 
+  def ensure_current_user
+    redirect_with_alert unless current_user.present?
+  end
+
   def question_params
     params.require(:question).permit(:body, :user_id)
   end
 
-  def set_question
-    @question = Question.find(params[:id])
+  def set_question_for_current_user
+    @question = current_user.questions.find(params[:id])
   end
 end
